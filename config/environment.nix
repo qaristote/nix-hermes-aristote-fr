a591a8af496c0d48beab750235de5d1067e41aec
Quentin Aristote <quentin@aristote.fr>
2021-08-07 11:09:36 +0200

    make git available by default

--- config/environment.nix
+++ config/environment.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ vim ];
+  environment.systemPackages = with pkgs; [ vim gitMinimal ];
   programs.bash.promptInit = ''
     PS1="\n\[\033[1;32m\][\[\e]0;\u@$(hostname -f): \w\a\]\u@$(hostname -f):\w]\$\[\033[0m\] '
   '';

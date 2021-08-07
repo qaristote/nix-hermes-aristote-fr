8e9bcb076c130031c6b1d6eddd2cf7e2b611b5b7
Quentin Aristote <quentin@aristote.fr>
2021-08-07 11:02:13 +0200

    use fqdn in bash prompt

--- config/environment.nix
+++ config/environment.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [ vim ];
   programs.bash.promptInit = ''
-    PS1='\n\[\033[1;32m\][\[\e]0;\u@\H: \w\a\]\u@\H:\w]\$\[\033[0m\] '
+    PS1="\n\[\033[1;32m\][\[\e]0;\u@$(hostname -f): \w\a\]\u@$(hostname -f):\w]\$\[\033[0m\] '
   '';
 
   i18n.defaultLocale = "en_US.UTF-8";

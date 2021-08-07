a2f14cb46f249ecb388252d3f9d88f1efe8e8227
Quentin Aristote <quentin@aristote.fr>
2021-08-07 18:47:45 +0200

    update academic webpage

--- pkgs/academic-webpage/default.nix
+++ pkgs/academic-webpage/default.nix
@@ -1,15 +1,16 @@
 { pkgs, stdenv, ... }:
 
-stdenv.mkDerivation {
-  name = "academic-webpage";
+stdenv.mkDerivation rec {
+  pname = "academic-webpage";
+  version = "2021-08-07";
 
   buildInputs = with pkgs; [ hugo ];
 
   src = pkgs.fetchFromGitHub {
     owner = "qaristote";
     repo = "academic-webpage";
-    rev = "18e00fdd22643831376e012793574d9293243f6f";
-    sha256 = "148gknjlwr71q5gkp4q7bnza64222izhn949ki14k7b9y7j486c6";
+    rev = "${version}";
+    sha256 = sha256:12f1ybxq6m9n5pr3cfx1d8svjw0f750pdy6b7rmv83i3gymsw1i0;
     fetchSubmodules = true;
   };
 

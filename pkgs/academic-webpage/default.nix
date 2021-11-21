7b8036351910372d13fe6746bdafc88f247a3b01
Quentin Aristote <quentin@aristote.fr>
2021-11-21 17:52:26 +0100

    update academic-webpage to version with github icon

--- pkgs/academic-webpage/default.nix
+++ pkgs/academic-webpage/default.nix
@@ -9,8 +9,8 @@ stdenv.mkDerivation rec {
   src = pkgs.fetchFromGitHub {
     owner = "qaristote";
     repo = "academic-webpage";
-    rev = "51211cc9521fc7ff32e9d0e0315a45904f909f15";
-    sha256 = "1874v3x7ks8lqiivvacdfqznlpnqizdk559h8kc5swlrh1pc7bzx";
+    rev = "1ee3b3878082a0c8fc5c72641d20ead89b01e8f3";
+    sha256 = "PDliiJNLnauVyEdssnhYReXZTunlmv6KNCgVNf6U+os=";
     fetchSubmodules = true;
   };
 

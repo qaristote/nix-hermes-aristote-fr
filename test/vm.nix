0b6558fbd828800621cd9e8918fb91f2602111ba
Quentin Aristote <quentin@aristote.fr>
2021-11-26 14:46:07 +0100

    write configuration for running in a local container

--- test/vm.nix
+++ test/vm.nix
@@ -1,6 +1,50 @@
-{ lib, ... }:
-{
-    imports = [ ../configuration.nix ];
+{ config, lib, ... }:
 
-    networking.domain = lib.mkForce "latitude7490";
+let
+  nginxPorts = lib.concatLists
+    (lib.mapAttrsToList (_: cfg: (builtins.map (x: x.port) cfg.listen))
+      config.services.nginx.virtualHosts);
+in {
+  imports = [ ../configuration.nix ];
+
+  networking = lib.mkForce {
+    domain = "aristote.vm";
+
+    interfaces = { };
+    defaultGateway = null;
+    nameservers = [ ];
+
+    firewall = { allowedTCPPorts = nginxPorts; };
+  };
+
+  services.nginx.virtualHosts = {
+    "quentin.${config.networking.domain}" = {
+      listen = lib.mkForce [{
+        addr = "0.0.0.0";
+        port = 8080;
+      }];
+      forceSSL = lib.mkForce false;
+      enableACME = lib.mkForce false;
+    };
+    "searx.${config.networking.domain}" = {
+      listen = lib.mkForce [{
+        addr = "0.0.0.0";
+        port = 8081;
+      }];
+      forceSSL = lib.mkForce false;
+      enableACME = lib.mkForce false;
+    };
+    "money.${config.networking.domain}" = {
+      listen = lib.mkForce [{
+        addr = "0.0.0.0";
+        port = 8082;
+      }];
+      forceSSL = lib.mkForce false;
+      enableACME = lib.mkForce false;
+    };
+  };
+
+  environment.etc."searx/secrets".text = ''
+    SECRET_KEY=secret_key
+  '';
 }

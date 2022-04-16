f3270745f9039f144b4f124f38bdbf4ad7e0ae4f
Quentin Aristote <quentin@aristote.fr>
2022-04-16 12:53:52 +0200

    academic-webpage: update to wowchemy v5

--- pkgs/academic-webpage/default.nix
+++ pkgs/academic-webpage/default.nix
@@ -1,22 +1,43 @@
-{ pkgs, stdenv, ... }:
+{ pkgs, stdenvNoCC, fetchFromGitHub, ... }:
 
-stdenv.mkDerivation rec {
+let wowchemy-module = name: stdenvNoCC.mkDerivation {
+      inherit name;
+      version = "v5.5.0";
+      src = fetchFromGitHub {
+        owner = "wowchemy";
+        repo = "wowchemy-hugo-themes";
+        rev = "3f178a06f49582758671432d3ff8298f5d65244f";
+        sha256 = "2+Pf8cBS/QEbsIjbk1VSG+OUqxHYKK7/kLajNoHvN1k=";
+      };
+      installPhase = ''
+        cp -r $src/"${name}" $out
+      '';
+      preferLocalBuild = true;
+    };
+    wowchemy = wowchemy-module "wowchemy";
+    wowchemy-cms = wowchemy-module "wowchemy-cms";
+
+in stdenvNoCC.mkDerivation rec {
   pname = "academic-webpage";
-  version = "2021-08-14";
+  version = "latest";
 
-  buildInputs = with pkgs; [ hugo ];
+  buildInputs = with pkgs; [ hugo wowchemy wowchemy-cms ];
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "qaristote";
     repo = "academic-webpage";
-    rev = "1ee3b3878082a0c8fc5c72641d20ead89b01e8f3";
-    sha256 = "PDliiJNLnauVyEdssnhYReXZTunlmv6KNCgVNf6U+os=";
-    fetchSubmodules = true;
+    rev = "bc012418ac3621a7292bce834d2efb837281bb15";
+    sha256 = "dCfTxHz98V7QOnYCP2f1QT/4UDgnUq4HnShY+q6eYxY=";
   };
 
-  phases = [ "unpackPhase" "buildPhase" ];
+  patchPhase = ''
+    sed -i -e "s/github.com\/wowchemy\/wowchemy-hugo-modules\/\(wowchemy[-a-z]*\)\/v5/\1/g" config/_default/config.yaml
+    mkdir -p themes
+    ln -sf "${wowchemy}" themes/wowchemy
+    ln -sf "${wowchemy-cms}" themes/wowchemy-cms
+  '';
 
-  buildPhase = ''
+  installPhase = ''
     hugo --destination $out
   '';
 }

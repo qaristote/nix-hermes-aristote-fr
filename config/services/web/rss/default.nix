dc278c9ef8377e2405698f17c0406c393465a65a
Quentin Aristote <quentin@aristote.fr>
2022-11-27 17:46:39 +0100

    services: web: rss: remove extra comments

--- config/services/web/rss/default.nix
+++ config/services/web/rss/default.nix
@@ -2,24 +2,9 @@
 
 let
   cfg = config.services.rss-bridge;
-  # debug = true;
-  # rss-bridge = pkgs.rss-bridge.overrideAttrs (oldAttrs:
-  #   oldAttrs // {
-  #     installPhase = oldAttrs.installPhase + ''
-  #       pushd $out/bridges
-  #       ln -sf ${./ParisJazzClubBridge.php} ParisJazzClubBridge.php
-  #       ln -sf ${./MaisonDeLaRadioBridge.php} MaisonDeLaRadioBridge.php
-  #       ln -sf ${./FipAlbumsBridge.php} FipAlbumsBridge.php
-  #       ln -sf ${./WhatsOnMubiBridge.php} WhatsOnMubiBridge.php
-  #       popd
-  #     '' + lib.optionalString debug ''
-  #       touch $out/DEBUG
-  #     '';
-  #   });
 in {
   services.rss-bridge = {
     enable = true;
-    # whitelist = [ "ParisJazzClub" "MaisonDeLaRadio" "FipAlbumsBridge" "WhatsOnMubi" ];
     extraBridges = [
       # Music
       {
@@ -47,7 +32,6 @@ in {
   services.nginx = lib.mkIf (cfg.virtualHost != null) {
     virtualHosts.${cfg.virtualHost} = {
       serverName = "rss.${config.networking.domain}";
-      # root = lib.mkForce "${rss-bridge}";
       forceSSL = true;
       enableACME = true;
     };

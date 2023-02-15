b1326e823b9cc23031c1dcf6656b3f93b88bbf8e
Quentin Aristote <quentin@aristote.fr>
2023-02-15 21:48:10 +0100

    automatically fetch (non-structural) website updates when updating the system

--- config/services/web/quentin/default.nix
+++ config/services/web/quentin/default.nix
@@ -7,4 +7,7 @@
     forceSSL = true;
     enableACME = true;
   };
+
+  # automatically fetch (non-structural) website updates when updating the system
+  system.autoUpgrade.flags = [ "--update-input" "personal-webpage/data" ];
 }

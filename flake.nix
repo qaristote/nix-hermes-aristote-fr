2e78e3aa4f3dd4706fa15bee8e0bbde58c9e5ce7
Quentin Aristote <quentin@aristote.fr>
2023-02-19 17:04:27 +0100

    format flake

--- flake.nix
+++ flake.nix
@@ -9,28 +9,33 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, my-nixpkgs, personal-webpage, ... }: {
-    nixosConfigurations = let
-      system = "x86_64-linux";
-      commonModules = [
-        my-nixpkgs.nixosModules.personal
-        ({ ... }: {
-          nixpkgs.overlays =
-            [ my-nixpkgs.overlays.personal personal-webpage.overlays.default (_: prev: {
-              inherit (nixpkgs-unstable.legacyPackages."${prev.system}") filtron;
-            })];
-        })
-      ];
-    in {
-      hermes = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = commonModules
-          ++ [ ./config ./config/hardware-configuration.nix ];
-      };
-      hermes-test = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = commonModules ++ [ ./tests/configuration.nix ];
+  outputs =
+    { self, nixpkgs, nixpkgs-unstable, my-nixpkgs, personal-webpage, ... }: {
+      nixosConfigurations = let
+        system = "x86_64-linux";
+        commonModules = [
+          my-nixpkgs.nixosModules.personal
+          ({ ... }: {
+            nixpkgs.overlays = [
+              my-nixpkgs.overlays.personal
+              personal-webpage.overlays.default
+              (_: prev: {
+                inherit (nixpkgs-unstable.legacyPackages."${prev.system}")
+                  filtron;
+              })
+            ];
+          })
+        ];
+      in {
+        hermes = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = commonModules
+            ++ [ ./config ./config/hardware-configuration.nix ];
+        };
+        hermes-test = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = commonModules ++ [ ./tests/configuration.nix ];
+        };
       };
     };
-  };
 }

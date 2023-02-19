fbfd89cc0b9f4146c2b4db5d1ab829ef2411c4bc
Quentin Aristote <quentin@aristote.fr>
2023-02-19 22:12:41 +0100

    config: nginx: enable text compression

--- config/services/web/default.nix
+++ config/services/web/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./money ./quentin ./rss ./searx ./webkeydirectory ];
@@ -10,6 +10,7 @@
 
   services.nginx = {
     enable = true;
+    additionalModules = [ pkgs.nginxModules.brotli ];
     # return 444 when trying to connect directly through the IP address
     virtualHosts."_" = {
       default = true;
@@ -20,6 +21,16 @@
     appendHttpConfig = ''
       types_hash_bucket_size 128;
       access_log /dev/null;
+
+      # compression
+      gzip on;
+      gzip_vary on;
+      gzip_proxied any;
+      gzip_comp_level 6;
+      gzip_types text/plain text/css text/xml application/json application/javascript application/xml+rss application/atom+xml image/svg+xml;
+      brotli on;
+      brotli_comp_level 6;
+      brotli_types text/xml image/svg+xml application/x-font-ttf image/vnd.microsoft.icon application/x-font-opentype application/json font/eot application/vnd.ms-fontobject application/javascript font/otf application/xml application/xhtml+xml text/javascript application/x-javascript text/plain application/x-font-truetype application/xml+rss image/x-icon font/opentype text/css image/x-win-bitmap;
     '';
   };
 }

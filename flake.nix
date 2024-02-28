3be45186577797f3c902d4e2a99d0df317855409
quentin@aristote.fr <quentin@aristote.fr>
2024-02-28 16:44:36 +0100

    nix: update 23.05->23.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
       inputs.nixpkgs.follows = "/nixpkgs";
     };
     my-nixpkgs.url = "github:qaristote/my-nixpkgs";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11-small";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   };
 

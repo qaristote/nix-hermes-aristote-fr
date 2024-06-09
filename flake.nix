2f7f9b768625616ef3afb4eeca2576368d3b2680
quentin@aristote.fr <quentin@aristote.fr>
2024-06-09 14:50:17 +0200

    update nixos to 24.05

--- flake.nix
+++ flake.nix
@@ -5,34 +5,39 @@
       inputs.nixpkgs.follows = "/nixpkgs";
     };
     my-nixpkgs.url = "github:qaristote/my-nixpkgs";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
   };
 
-  outputs =
-    { self, nixpkgs, my-nixpkgs, personal-webpage, ... }: {
-      nixosConfigurations = let
-        system = "x86_64-linux";
-        commonModules = [
-          my-nixpkgs.nixosModules.personal
-          ({ ... }: {
-            nixpkgs.overlays = [
-              (self: _: { personal = { inherit (personal-webpage.packages."${self.system}") webpage; };})
-              # TODO the order shouldn't matter, yet this overlay doesn't work
-              # if it comes first
-              my-nixpkgs.overlays.personal
-            ];
-          })
-        ];
-      in {
-        hermes = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = commonModules
-            ++ [ ./config ./config/hardware-configuration.nix ];
-        };
-        hermes-test = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = commonModules ++ [ ./tests/configuration.nix ];
-        };
+  outputs = {
+    nixpkgs,
+    my-nixpkgs,
+    personal-webpage,
+    ...
+  }: {
+    nixosConfigurations = let
+      system = "x86_64-linux";
+      commonModules = [
+        my-nixpkgs.nixosModules.personal
+        ({...}: {
+          nixpkgs.overlays = [
+            (self: _: {personal = {inherit (personal-webpage.packages."${self.system}") webpage;};})
+            # TODO the order shouldn't matter, yet this overlay doesn't work
+            # if it comes first
+            my-nixpkgs.overlays.personal
+          ];
+        })
+      ];
+    in {
+      hermes = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules =
+          commonModules
+          ++ [./config ./config/hardware-configuration.nix];
+      };
+      hermes-test = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = commonModules ++ [./tests/configuration.nix];
       };
     };
+  };
 }

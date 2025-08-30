1eaf79c34b965327cebbbcb346b90612e45c1181
quentin@aristote.fr <quentin@aristote.fr>
2025-08-30 13:40:02 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'my-nixpkgs':
        'github:qaristote/my-nixpkgs/e533f93455fefa2314d5f76066736eb58258d943' (2025-08-24)
      → 'github:qaristote/my-nixpkgs/230bc2319c9e068638ca2191759617b9a331f357' (2025-08-30)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/4e7667a90c167f7a81d906e5a75cba4ad8bee620' (2025-08-26)
      → 'github:NixOS/nixpkgs/41d292bfc37309790f70f4c120b79280ce40af16' (2025-08-29)

--- config/environment.nix
+++ config/environment.nix
@@ -1,11 +1,7 @@
-{ pkgs, ... }:
-
+{ ... }:
 {
   personal.environment = {
     enable = true;
     locale.enable = true;
   };
-  programs.bash.promptInit = ''
-    PS1="\n\[\033[1;32m\][\[\e]0;\u@$(hostname -f): \w\a\]\u@$(hostname -f):\w]\$\[\033[0m\] "
-  '';
 }

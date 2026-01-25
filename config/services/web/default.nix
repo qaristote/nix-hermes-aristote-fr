1368479a9c40e591b88e1a1a0b545d37ac19fb28
quentin@aristote.fr <quentin@aristote.fr>
2026-01-25 21:38:37 +0100

    my-nixpkgs: fix default nginx config
    Flake lock file updates:
    
    • Updated input 'my-nixpkgs':
        'github:qaristote/my-nixpkgs/aa8af127cef35820a05a9d2c3ab371fa3ddc4795' (2026-01-24)
      → 'github:qaristote/my-nixpkgs/2fd9ddfd4925f85dda9ba2fd7fb5ec5c07864c45' (2026-01-25)

--- config/services/web/default.nix
+++ config/services/web/default.nix
@@ -14,6 +14,15 @@
     defaults.email = "quentin@aristote.fr";
   };
 
-  services.nginx.enable = true;
+  services.nginx = {
+    enable = true;
+    # return 444 when trying to connect to some unknown virtual host
+    virtualHosts."_" = {
+      default = true;
+      extraConfig = ''
+        return 444;
+      '';
+    };
+  };
   systemd.services.nginx.personal.monitor = true;
 }

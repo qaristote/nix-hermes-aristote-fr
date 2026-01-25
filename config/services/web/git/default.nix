b47096a17eb16c03536a36e6713a6f53d2a33295
quentin@aristote.fr <quentin@aristote.fr>
2026-01-25 23:05:43 +0100

    cgit proxy: disable compression for subfiltering

--- config/services/web/git/default.nix
+++ config/services/web/git/default.nix
@@ -10,6 +10,8 @@
         proxyPass = "http://hephaistos.aristote.mesh/git/";
         extraConfig = ''
           proxy_redirect default;
+          # fix internal hyperlinks
+          proxy_set_header Accept-Encoding ""; 
           sub_filter_once off;
           sub_filter 'href=\'/git/' 'href=\'/';
           sub_filter 'action=\'/git/' 'action=\'/';
